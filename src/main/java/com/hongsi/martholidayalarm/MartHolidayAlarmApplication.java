a931d4c8c30dbe66925e2f4ef365b74e3484a03e
Sihong Park <pozt1234@gmail.com>
2018-03-20 03:29:57 +0900

    프로젝트 생성

---
+++ src/main/java/com/hongsi/martholidayalarm/MartHolidayAlarmApplication.java
@@ -0,0 +1,12 @@
+package com.hongsi.martholidayalarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MartHolidayAlarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MartHolidayAlarmApplication.class, args);
+	}
+}
