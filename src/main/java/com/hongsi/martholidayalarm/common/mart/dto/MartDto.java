24b74cd17bf88d0fa664e4b5a1c687a580fbcee7
Sihong Park <pozt1234@gmail.com>
2018-08-15 01:40:55 +0900

    MartDto 프로퍼티 추가
    - 생성일, 마지막 수정일 프로퍼티 추가

--- src/main/java/com/hongsi/martholidayalarm/common/mart/dto/MartDto.java
+++ src/main/java/com/hongsi/martholidayalarm/common/mart/dto/MartDto.java
@@ -4,8 +4,11 @@ import com.hongsi.martholidayalarm.common.mart.domain.Holiday;
 import com.hongsi.martholidayalarm.common.mart.domain.Mart;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import javax.validation.constraints.NotBlank;
 import lombok.Getter;
 
@@ -13,36 +16,47 @@ import lombok.Getter;
 @ApiModel(value = "마트 정보")
 public class MartDto {
 
+	public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter
+			.ofPattern("yyyy-MM-dd HH24:MI:SS").withLocale(Locale.KOREAN);
+
 	@ApiModelProperty(value = "고유 아이디", dataType = "java.lang.Long", required = true, position = 1)
 	private Long id;
 
-	@ApiModelProperty(value = "마트타입", dataType = "java.lang.String", required = true, position = 2, example = "마트 한글명 ex) 이마트")
+	@ApiModelProperty(value = "생성일", dataType = "java.lang.String", required = true, position = 2)
+	private String createdDate;
+
+	@ApiModelProperty(value = "마지막 수정일", dataType = "java.lang.String", required = true, position = 3)
+	private String modifiedDate;
+
+	@ApiModelProperty(value = "마트타입", dataType = "java.lang.String", required = true, position = 4, example = "마트 한글명 ex) 이마트")
 	@NotBlank
 	private String martType;
 
-	@ApiModelProperty(value = "지점명", dataType = "java.lang.String", required = true, position = 3)
+	@ApiModelProperty(value = "지점명", dataType = "java.lang.String", required = true, position = 5)
 	private String branchName;
 
-	@ApiModelProperty(value = "지역", dataType = "java.lang.String", required = true, position = 4)
+	@ApiModelProperty(value = "지역", dataType = "java.lang.String", required = true, position = 6)
 	private String region;
 
-	@ApiModelProperty(value = "전화번호", dataType = "java.lang.String", required = true, position = 5)
+	@ApiModelProperty(value = "전화번호", dataType = "java.lang.String", required = true, position = 7)
 	private String phoneNumber;
 
-	@ApiModelProperty(value = "주소", dataType = "java.lang.String", required = true, position = 6)
+	@ApiModelProperty(value = "주소", dataType = "java.lang.String", required = true, position = 8)
 	private String address;
 
-	@ApiModelProperty(value = "영업시간", dataType = "java.lang.String", position = 7, allowEmptyValue = true)
+	@ApiModelProperty(value = "영업시간", dataType = "java.lang.String", position = 9, allowEmptyValue = true)
 	private String openingHours;
 
-	@ApiModelProperty(value = "마트 url", dataType = "java.lang.String", position = 8, allowEmptyValue = true)
+	@ApiModelProperty(value = "마트 url", dataType = "java.lang.String", position = 10, allowEmptyValue = true)
 	private String url;
 
-	@ApiModelProperty(value = "휴무일", dataType = "java.lang.String", position = 9, allowEmptyValue = true)
+	@ApiModelProperty(value = "휴무일", dataType = "java.lang.String", position = 11, allowEmptyValue = true)
 	private List<String> holidays;
 
 	public MartDto(Mart entity) {
 		id = entity.getId();
+		createdDate = formatLocalDateTime(entity.getCreatedDate());
+		modifiedDate = formatLocalDateTime(entity.getModifiedDate());
 		martType = entity.getMartType().getName();
 		branchName = entity.getBranchName();
 		region = entity.getRegion();
@@ -51,6 +65,10 @@ public class MartDto {
 		openingHours = entity.getOpeningHours();
 		url = entity.getUrl();
 		holidays = toHolidayDto(entity.getHolidays());
+	}
+
+	private String formatLocalDateTime(LocalDateTime localDateTime) {
+		return localDateTime.format(DATE_TIME_FORMATTER);
 	}
 
 	private List<String> toHolidayDto(List<Holiday> holidays) {
