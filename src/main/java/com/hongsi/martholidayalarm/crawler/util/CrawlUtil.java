f9cc72efdb42b77d7263402eff5840941223218e
Sihong Park <pozt1234@gmail.com>
2018-11-07 16:56:46 +0900

    refactor: Pattern을 클래스 상수로 변경

--- src/main/java/com/hongsi/martholidayalarm/crawler/util/CrawlUtil.java
+++ src/main/java/com/hongsi/martholidayalarm/crawler/util/CrawlUtil.java
@@ -5,14 +5,14 @@ import java.util.regex.Pattern;
 
 public class CrawlUtil {
 
-	private static final Pattern replacingRegionPattern = Pattern.compile("(경상|충청|전라)(남도|북도)");
+	private static final Pattern REPLACING_REGION_PATTERN = Pattern.compile("(경상|충청|전라)(남도|북도)");
 
 	private CrawlUtil() {
 	}
 
 	public static String getRegionFromAddress(String address) {
 		String region = address.substring(0, 2);
-		Matcher matcher = replacingRegionPattern.matcher(address);
+		Matcher matcher = REPLACING_REGION_PATTERN.matcher(address);
 		if (matcher.find()) {
 			region = matcher.group(1).substring(0, 1) + matcher.group(2).substring(0, 1);
 		}
--- src/main/java/com/hongsi/martholidayalarm/crawler/util/CrawlingValidationUtil.java
+++ src/main/java/com/hongsi/martholidayalarm/crawler/util/CrawlingValidationUtil.java
@@ -5,9 +5,11 @@ import java.util.regex.Pattern;
 
 public class CrawlingValidationUtil {
 
+	public static final Pattern PHONE_NUMBER_PATTERN = Pattern
+			.compile("^\\d{2,3}-\\d{3,4}-\\d{4}$");
+
 	public static boolean isValidPhoneNumber(String phoneNumber) {
-		Pattern phoneNumberPattern = Pattern.compile("^\\d{2,3}-\\d{3,4}-\\d{4}$");
-		Matcher matcher = phoneNumberPattern.matcher(phoneNumber);
+		Matcher matcher = PHONE_NUMBER_PATTERN.matcher(phoneNumber);
 		return matcher.matches();
 	}
 }
