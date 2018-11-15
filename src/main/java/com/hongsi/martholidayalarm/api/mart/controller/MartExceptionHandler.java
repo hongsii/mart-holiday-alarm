242c173eb38a771ce3b40a4b46fd4f44e407e157
Sihong Park <pozt1234@gmail.com>
2018-11-15 17:17:49 +0900

    refactor: MartExceptionHandler 클래스 추출

--- src/main/java/com/hongsi/martholidayalarm/api/mart/controller/MartExceptionHandler.java
+++ src/main/java/com/hongsi/martholidayalarm/api/mart/controller/MartExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.hongsi.martholidayalarm.api.mart.controller;
 
+import com.hongsi.martholidayalarm.api.exception.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,6 +13,12 @@ public class MartExceptionHandler {
 	@ExceptionHandler(IllegalArgumentException.class)
 	@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "This api request has invalid parameters, check parameters")
 	public ResponseEntity<String> handleBadRequest(IllegalArgumentException exception) {
-		return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(ResourceNotFoundException.class)
+	@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Not found resource of request")
+	public ResponseEntity<String> handleResourceNotFound(ResourceNotFoundException exception) {
+		return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
 	}
 }
