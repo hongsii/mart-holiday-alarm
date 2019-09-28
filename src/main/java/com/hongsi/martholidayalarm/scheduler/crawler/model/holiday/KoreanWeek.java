d8e743927486ff7d3927ea026f0c914aa610ebca
hongsii <hongsii.dev@gmail.com>
2019-09-29 02:57:41 +0900

    :recycle: KoreanWeek spock 테스트로 변경

--- src/main/java/com/hongsi/martholidayalarm/scheduler/crawler/model/holiday/KoreanWeek.java
+++ src/main/java/com/hongsi/martholidayalarm/scheduler/crawler/model/holiday/KoreanWeek.java
@@ -9,50 +9,44 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
-import static java.util.Arrays.asList;
-
 public enum KoreanWeek {
 
 	FIRST("첫"), SECOND("둘", "두"), THIRD("셋", "세"), FOURTH("넷", "네"), FIFTH("다섯");
 
 	private static final String REGEX_DELIMITER = "|";
-	private static final Pattern MATCH_CHARACTER_PATTERN = Pattern.compile(
+	private static final Pattern WEEK_PATTERN = Pattern.compile(
 			Arrays.stream(values())
-					.flatMap(weekWrapper -> weekWrapper.getMatchCharacters().stream())
+					.flatMap(koreanWeek -> koreanWeek.characters.stream())
 					.collect(Collectors.joining(REGEX_DELIMITER))
 	);
 
-	private List<String> matchCharacters = new ArrayList<>();
+	private List<String> characters;
 
-	KoreanWeek(String... matchCharacter) {
-		matchCharacters.addAll(asList(matchCharacter));
-		matchCharacters.add(Integer.toString(getWeek()));
+	KoreanWeek(String... characters) {
+		this.characters = new ArrayList<>(characters.length + 1);
+		this.characters.addAll(Arrays.asList(characters));
+		this.characters.add(Integer.toString(getWeek()));
 	}
 
 	public static KoreanWeek of(String text) {
 		return Arrays.stream(values())
 				.filter(weekWrapper -> weekWrapper.hasCharacter(text))
 				.findFirst()
-				.orElseThrow(() -> new IllegalArgumentException(
-						"Not found week of this text : " + text));
+				.orElseThrow(() -> new IllegalArgumentException("Not found week of this text : " + text));
 	}
 
 	public static List<KoreanWeek> parseToCollection(String text) {
-		Matcher matcher = MATCH_CHARACTER_PATTERN.matcher(text);
+		Matcher matcher = WEEK_PATTERN.matcher(text);
 		return MatchSpliterator.from(matcher).stream()
 				.map(KoreanWeek::of)
 				.collect(Collectors.toList());
 	}
 
-	private List<String> getMatchCharacters() {
-		return matchCharacters;
-	}
-
-	public boolean hasCharacter(String text) {
-		return matchCharacters.contains(text);
-	}
-
 	public int getWeek() {
 		return ordinal() + 1;
+	}
+
+	private boolean hasCharacter(String text) {
+		return characters.contains(text);
 	}
 }
