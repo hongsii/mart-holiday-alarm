55dabd13caee48d69f2f33acc64433d2ff7f63fa
hongsii <hongsii.dev@gmail.com>
2019-10-04 02:10:05 +0900

    :white_check_mark: RegularHoliday 테스트 추가

--- src/test/groovy/com/hongsi/martholidayalarm/scheduler/crawler/model/holiday/RegularHolidayTest.groovy
+++ src/test/groovy/com/hongsi/martholidayalarm/scheduler/crawler/model/holiday/RegularHolidayTest.groovy
@@ -2,14 +2,32 @@ package com.hongsi.martholidayalarm.scheduler.crawler.model.holiday
 
 import spock.lang.Specification
 
+import static com.hongsi.martholidayalarm.scheduler.crawler.model.holiday.KoreanDayOfWeek.MONDAY
+import static com.hongsi.martholidayalarm.scheduler.crawler.model.holiday.KoreanDayOfWeek.SUNDAY
+import static com.hongsi.martholidayalarm.scheduler.crawler.model.holiday.KoreanWeek.FIRST
+
 class RegularHolidayTest extends Specification {
 
     def "Should be equal holiday with same week and day of week"() {
         given:
-        def week = KoreanWeek.FIRST
-        def dayOfWeek = KoreanDayOfWeek.SUNDAY
+        def week = FIRST
+        def dayOfWeek = SUNDAY
 
         expect:
         RegularHoliday.of(week, dayOfWeek) == RegularHoliday.of(week, dayOfWeek)
+    }
+
+    def "Shouldn't be created, if all parameter don't exist"() {
+        when:
+        RegularHoliday.of(koreanWeek, koreanDayOfWeek)
+
+        then:
+        thrown(IllegalArgumentException)
+
+        where:
+        koreanWeek | koreanDayOfWeek | _
+        null       | null            | _
+        FIRST      | null            | _
+        null       | MONDAY          | _
     }
 }
