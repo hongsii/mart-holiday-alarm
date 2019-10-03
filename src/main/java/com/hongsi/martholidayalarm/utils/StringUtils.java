b21f2163d03fc0a95c2bdd298c7dd973db26361d
hongsii <hongsii.dev@gmail.com>
2019-10-04 02:32:11 +0900

    :sparkles: 빈 문자열을 확인하는 유틸 메서드 추가
    - StringUtils.isBlank() 추가
    - Spock 테스트로 변경

--- src/main/java/com/hongsi/martholidayalarm/utils/StringUtils.java
+++ src/main/java/com/hongsi/martholidayalarm/utils/StringUtils.java
@@ -2,7 +2,11 @@ package com.hongsi.martholidayalarm.utils;
 
 public class StringUtils {
 
-	public static String replaceWhitespace(String str) {
-		return str.replaceAll("\\s+", "");
+	public static String replaceWhitespace(String value) {
+		return value.replaceAll("\\s+", "");
+	}
+
+	public static boolean isBlank(String value) {
+		return value == null || value.trim().isEmpty();
 	}
 }
