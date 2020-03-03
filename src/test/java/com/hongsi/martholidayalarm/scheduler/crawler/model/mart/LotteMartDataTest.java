1b7c6fc397e03f014aceb6085003b5054029e9ad
hongsii <hongsii.dev@gmail.com>
2020-03-03 10:56:37 +0900

    깨진 테스트 수정

--- src/test/java/com/hongsi/martholidayalarm/scheduler/crawler/model/mart/LotteMartDataTest.java
+++ src/test/java/com/hongsi/martholidayalarm/scheduler/crawler/model/mart/LotteMartDataTest.java
@@ -3,6 +3,7 @@ package com.hongsi.martholidayalarm.scheduler.crawler.model.mart;
 import com.hongsi.martholidayalarm.domain.mart.Holiday;
 import org.junit.Test;
 
+import java.time.Year;
 import java.util.List;
 
 import static java.util.Arrays.asList;
@@ -15,9 +16,10 @@ public class LotteMartDataTest {
 		LotteMartData lotteMartData = new LotteMartData();
 		lotteMartData.setHolidayText("※2/5(화요일) 설날 당일 휴무!! 2월10일 일요일 정상영업, 2월24일 정기휴무");
 
-		Holiday holiday1 = Holiday.of(2019, 2, 5),
-				holiday2 = Holiday.of(2019, 2, 10),
-				holiday3 = Holiday.of(2019, 2, 24);
+		int currentYear = Year.now().getValue();
+		Holiday holiday1 = Holiday.of(currentYear, 2, 5),
+				holiday2 = Holiday.of(currentYear, 2, 10),
+				holiday3 = Holiday.of(currentYear, 2, 24);
 
 		List<Holiday> holidays = lotteMartData.excludeHoliday(
 				asList(holiday1, holiday2, holiday3)
